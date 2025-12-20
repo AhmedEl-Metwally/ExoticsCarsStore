c70abbdcc789841734eeef975275a3711efa904a
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-12-20 17:23:37 +0200

    Basket Module -Entities

---
+++ ExoticsCarsStoreServerSide.Domain/Models/BasketModule/CustomerBasket.cs
@@ -0,0 +1,8 @@
+﻿namespace ExoticsCarsStoreServerSide.Domain.Models.BasketModule
+{
+    public class CustomerBasket
+    {
+        public string Id { get; set; } = default!;
+        public ICollection<BasketItem> BasketItems { get; set; } = new List<BasketItem>();
+    }
+}
