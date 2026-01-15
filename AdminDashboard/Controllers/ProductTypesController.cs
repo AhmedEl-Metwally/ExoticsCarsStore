85a18105b1150c0fcdc6ec3db2164ac4efafc725
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2026-01-15 19:26:57 +0200

    Product Types Management

---
+++ AdminDashboard/Controllers/ProductTypesController.cs
@@ -0,0 +1,37 @@
+﻿using ExoticsCarsStoreServerSide.Domain.Models.ProductModule;
+using ExoticsCarsStoreServerSide.Domain.Specifications;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminDashboard.Controllers
+{
+    public class ProductTypesController(IUnitOfWork _unitOfWork) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var types = await _unitOfWork.GetRepository<ProductType, int>().GetAllAsync();
+            return View(types);
+        }
+
+        public async Task<IActionResult> Create(ProductType type)
+        {
+            try
+            {
+                await _unitOfWork.GetRepository<ProductType, int>().AddAsync(type);
+                await _unitOfWork.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("Name", "Please enter new name");
+                return View("Index", await _unitOfWork.GetRepository<ProductType, int>().GetAllAsync());
+            }
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var types = await _unitOfWork.GetRepository<ProductType, int>().GetByIdAsync(id);
+            _unitOfWork.GetRepository<ProductType, int>().Remove(types);
+            await _unitOfWork.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
